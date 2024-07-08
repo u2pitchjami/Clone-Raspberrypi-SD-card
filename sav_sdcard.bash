24a67f88c72945f1f0977d006de9df08d878e069
TB <149841209+u2pitchjami@users.noreply.github.com>
2024-07-08 16:27:19 +0200

    update and cleanup

--- sav_sdcard.bash
+++ sav_sdcard.bash
@@ -1,21 +1,23 @@
 #!/bin/bash
-#variables
+
+######CONFIGURATION###########################################################
 NOMSAV="sdcard" #nom du fichier de sauvegarde
-CHEMINSAV="/mnt/nfs/Documents/sav/pipoberrypi" #chemin où sera sauvegarder l'image
-SDCARD="/dev/mmcblk0" #chemin du support à sauvegarder
+CHEMINSAV="/path/to/where/sav/img" #chemin où sera sauvegarder l'image
+CHEMINPISHRINK="/path/where/is/pishrink.sh" #chemin pishrink
+SDCARD="/path/to/sdcard" #chemin du support à sauvegarder
 DATE=$(date "+%y%m%d")
 LOG=savpipoberrypi.txt
-DOSSIERLOGS="/mnt/nfs/Documents/scripts/logs/${DATE}${LOG}" #logs
+DOSSIERLOGS="/path/to/logs/${DATE}${LOG}" #logs
 NBSAV=7 #nb de sauvegardes à conserver
+######CONFIGURATION###########################################################
+
 echo "[`date`] sauvegarde de la carte SD" | tee -a "$DOSSIERLOGS" 
-#ssh -p 2100 -t "$SSHAUTH" "sudo dd if="${SDCARD}" bs=4M" status=progress | dd of="${CHEMINSAV}/${NOMSAV}_temp.img" conv=fsync status=progress >> "$DOSSIERLOGS"$DATE"$LOG" 2>&1
 sudo dd if="${SDCARD}" bs=4M status=progress of="${CHEMINSAV}/${NOMSAV}_temp.img" conv=fsync status=progress >> "$DOSSIERLOGS" 2>&1
 echo "[`date`] sauvegarde realisee" | tee -a "$DOSSIERLOGS"
 echo "[`date`] Demarrage de la compression avec pishrink"| tee -a "$DOSSIERLOGS"
 #lancement du script pischrink.sh
-sudo ./pishrink.sh -z "${CHEMINSAV}/${NOMSAV}_temp.img"
-#compression de l'image
-#tar -czf "${CHEMINSAV}/${NOMSAV}_temp.tar.gz" "${CHEMINSAV}/${NOMSAV}_temp.img.gz" | tee -a "$DOSSIERLOGS" 
+sudo ${CHEMINPISHRINK} -z "${CHEMINSAV}/${NOMSAV}_temp.img"
+
 echo "[`date`] controle de l'archive" | tee -a "$DOSSIERLOGS" 
 #contrôle de l'image
   if gzip -t "${CHEMINSAV}/${NOMSAV}_temp.img.gz"; then 
@@ -25,13 +27,16 @@ echo "[`date`] controle de l'archive" | tee -a "$DOSSIERLOGS"
     do
     D=$(( c - 1 ))
       if [ -f "${CHEMINSAV}/${NOMSAV}${D}.img.gz" ]; then
-      mv "${CHEMINSAV}/${NOMSAV}${D}.img.gz" "${CHEMINSAV}/${NOMSAV}${c}.img.gz"
+echo "fichier :  $c"
+echo "nom complet avant : ${CHEMINSAV}/${NOMSAV}${D}.img.gz"
+ mv "${CHEMINSAV}/${NOMSAV}${D}.img.gz" "${CHEMINSAV}/${NOMSAV}${c}.img.gz"
+echo "nom complet apres : ${CHEMINSAV}/${NOMSAV}${c}.img.gz" 
       fi
     done
 #renommage du fichier temporaire en sauvegarde normale
 echo "[`date`] Rennomage des fichiers"| tee -a "$DOSSIERLOGS"
 mv "${CHEMINSAV}/${NOMSAV}_temp.img.gz" "${CHEMINSAV}/${NOMSAV}0.img.gz"
 echo "[`date`] Voila c'est fini"| tee -a "$DOSSIERLOGS" 
-else 
+  else 
 echo "[`date`] archive corrompue, operation avortee" | tee -a "$DOSSIERLOGS"
-fi
+  fi
